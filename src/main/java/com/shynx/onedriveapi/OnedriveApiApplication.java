65020d7a05e0b2e8b41fb7c08450a77b8c3f050e
@felipe.micheletti_hadrons <felipe.micheletti_hadrons@prestador.globo>
2026-04-30 17:42:54 -0300

    Init: Created by Spring Initializr

---
+++ src/main/java/com/shynx/onedriveapi/OnedriveApiApplication.java
@@ -0,0 +1,13 @@
+package com.shynx.onedriveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnedriveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnedriveApiApplication.class, args);
+	}
+
+}
